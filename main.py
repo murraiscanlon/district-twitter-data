6505cb69431076a4d34eeb3a9379db6e5137cf5c
murrai.scanlon <murrai.scanlon@misd.org>
2021-03-31 12:41:03 -0500

    pulling flu tweets

--- main.py
+++ main.py
@@ -17,17 +17,33 @@ number_of_tweets = 200
 tweets = []
 likes = []
 time = []
+coordinates = []
+geo = []
 
-for i in tweepy.Cursor(api.user_timeline, id="murraiscanlon", tweet_mode="extended").items(number_of_tweets):
+# for i in tweepy.Cursor(api.user_timeline, id="murraiscanlon", tweet_mode="extended").items(number_of_tweets):
+#     tweets.append(i.full_text)
+#     likes.append(i.favorite_count)
+#     time.append(i.created_at)
+
+# for i in tweepy.Cursor(api.search, q="flu", tweet_mode="extended").items(number_of_tweets):
+#     tweets.append(i.full_text)
+#     likes.append(i.favorite_count)
+#     time.append(i.created_at)
+
+
+for i in tweepy.Cursor(api.search, q="flu", tweet_mode="extended").items(number_of_tweets):
     tweets.append(i.full_text)
-    likes.append(i.favorite_count)
-    time.append(i.created_at)
+    coordinates.append(i.coordinates)
+    geo.append(i.geo)
 
-df = pd.DataFrame({'tweets':tweets, 'likes':likes, 'time':time})
+
+df = pd.DataFrame({'tweets':tweets, 'coordinates':coordinates, 'geo':geo})
 print(df)
+
+
 # cursor = tweepy.Cursor(api.search, q='flu',tweet_mode="extended").items(1)
 # for i in cursor:
-#     # print(dir(i))
+#     print(dir(i))
 #     print(i.full_text)
 
 
